d2fe5a2c7df83e143cd05bba5de181da9f4cbbda
HOUNNOU N E RONALD BILL <eltonhounnou2@gmail.com>
2025-10-03 22:19:09 +0200

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+
 
 
 const queryClient = new QueryClient();
@@ -14,6 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
