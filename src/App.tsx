3482c1960bb938cac770212b9ca4cda640cf022b
HOUNNOU N E RONALD BILL <eltonhounnou2@gmail.com>
2025-11-07 12:46:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,35 @@ import { Analytics } from "@vercel/analytics/react";
 import { HelmetProvider } from "react-helmet-async";
 import SEO from "@/components/SEO";
 
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-import { lazy, Suspense } from "react";
-
-// Lazy loading 
-const Projects = lazy(() => import("./components/Projects"));
-const Skills = lazy(() => import("./components/Skills"));
-const Team = lazy(() => import("./components/Team"));
-
-
-const HeavyComponent = () => <div></div>;
 
 const queryClient = new QueryClient();
 
 const App = () => (
+  /
   <QueryClientProvider client={queryClient}>
+    
     <HelmetProvider>
+     
       <TooltipProvider>
+        
         <Toaster />
         <Sonner />
         <SEO />
+
+        
         <BrowserRouter>
-          
-            
-            <Suspense>
-              <Projects />
-              <Skills />
-              <Team />
-              <HeavyComponent />
-            </Suspense>
-          
+          {}
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+
+        
         <Analytics />
       </TooltipProvider>
     </HelmetProvider>
